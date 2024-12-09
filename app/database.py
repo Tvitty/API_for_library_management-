ad0b18cdf41f185687e20412087d89071a97c496
Vladislav Turnaev <Vladislaw.turnaev@yandex.ru>
2024-12-09 19:33:57 +0200

    Add database

---
+++ app/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from app.config import Settings
+
+
+
+DATA_BASE_URL = f"postgresql+asyncpg://{Settings.DB_USER}:{Settings.DB_PASS}@{Settings.DB_HOST}:{Settings.DB_PORT}/{Settings.DB_NAME}"
+
+engine = create_async_engine(DATA_BASE_URL)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
