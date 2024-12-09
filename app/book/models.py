7e55463872cf0905e049a238d45c46d676f57637
Vladislav Turnaev <Vladislaw.turnaev@yandex.ru>
2024-12-09 19:29:54 +0200

    Add models book

---
+++ app/book/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from app.database import Base
+
+
+class Book(Base):
+    __tablename__ = "book"
+
+    id = Column(Integer, primary_key=True)
+    book_title = Column(String, nullable=False)
+    description = Column(String)
+    author_id = Column(ForeignKey("author.id"))
+    available_copies = Column(Integer, nullable=False)
