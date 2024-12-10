f64337e792ce09d831760c38cee0e706c75f1cfd
Vladislav Turnaev <Vladislaw.turnaev@yandex.ru>
2024-12-10 09:08:30 +0200

    settings as s

--- app/database.py
+++ app/database.py
@@ -1,14 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-from app.config import settings
+from app.config import settings as s
 
-
-
-DATA_BASE_URL = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+DATA_BASE_URL = f"postgresql+asyncpg://{s.DB_USER}:{s.DB_PASS}@{s.DB_HOST}:{s.DB_PORT}/{s.DB_NAME}"
 
 engine = create_async_engine(DATA_BASE_URL)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
 
 class Base(DeclarativeBase):
     pass
